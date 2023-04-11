0f6e53e4d6fe44d3dd3fc66318ec9c8d1a501753
soi-ha <soy2302ten@gmail.com>
2023-04-11 15:37:57 +0900

    Component로 만들기

---
+++ week3/src/components/Banner.jsx
@@ -0,0 +1,33 @@
+export default function Banner() {
+	return (
+		<div className="banner">
+			<div className="banner-img">
+				<img
+					className="view"
+					src="https://product-image.kurly.com/cdn-cgi/image/quality=85/banner/main/pc/img/e6bbfa0b-004d-4b9c-923b-02c4521af50d.jpg"
+					alt="광고배너"
+				/>
+				<img
+					className="non-view"
+					src="https://product-image.kurly.com/cdn-cgi/image/quality=85/banner/main/pc/img/20139e33-d871-4de9-a2e8-18a3024af36d.jpg"
+					alt="광고배너"
+				/>
+				<img
+					className="non-view"
+					src="https://product-image.kurly.com/cdn-cgi/image/quality=85/banner/main/pc/img/fe5252d5-1af1-4e88-8c25-1e2c22beda61.jpg"
+					alt="광고배너"
+				/>
+				<img
+					className="non-view"
+					src="https://product-image.kurly.com/cdn-cgi/image/quality=85/banner/main/pc/img/7bf13b1c-a08b-495f-af5d-56cfa900eb39.jpg"
+					alt="광고배너"
+				/>
+				<img
+					className="non-view"
+					src="https://product-image.kurly.com/cdn-cgi/image/quality=85/banner/main/pc/img/ef5da663-4c2a-48cf-be5a-b39cc452b5b2.jpg"
+					alt="광고배너"
+				/>
+			</div>
+		</div>
+	);
+}
---
+++ week3/src/components/Main.jsx
@@ -0,0 +1,31 @@
+import Banner from './Banner';
+import Product from './Product';
+import Collection from './Collection';
+export default function Main() {
+	return (
+		<>
+			<div className="shadow"></div>
+			<div className="main">
+				<Banner />
+				<div className="main-box recommend">
+					<div className="box-title">이 상품 어때요?</div>
+					<div className="box-content">
+						<Product />
+						<Product />
+						<Product />
+						<Product />
+					</div>
+				</div>
+				<div className="main-box notice">
+					<img
+						src="https://product-image.kurly.com/banner/random-band/pc/img/df7fad64-8210-4f23-ada1-5088a348a959.jpg"
+						alt=""
+					/>
+				</div>
+				<div className="main-box collection">
+					<Collection />
+				</div>
+			</div>
+		</>
+	);
+}
