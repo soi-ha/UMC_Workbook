4dc079a0742e2c55cf4b8dce547f585b84ce2d9f
soi-ha <soy2302ten@gmail.com>
2023-05-10 23:17:30 +0900

    redux로 todolist 상태 관리

---
+++ todolist/src/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
+
+serviceWorker.unregister();
