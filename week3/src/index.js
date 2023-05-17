98de53fad55dfaf0c23b5aa36ee886593e654efe
soi-ha <soy2302ten@gmail.com>
2023-05-17 18:17:37 +0900

    .

--- week3/src/index.js
+++ week3/src/index.js
@@ -3,12 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// function reducer(state = {closePop, closeTodayPop} , action) {
+//   return state
+// }
+
+// let store = createStore(reducer)
+
+// const closePop = () => {
+//   setShowMainPop(false);
+// };
+
+// const closeTodayPop = () => {
+//   // 현재 시간을 담는 변수 expires
+//   let expires = new Date();
+//   // 현재 시간에서 24간 뒤의 시간을 저장
+//   expires = expires.setHours(expires.getHours() + 24);
+//   // 현재 시간의 24시간 뒤(value)의 시간을 homeVisited(key)에 저장
+//   localStorage.setItem('homeVisited', expires);
+//   // popup 노출 안함
+//   setShowMainPop(false);
+// };
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- week3/src/pages/Market.jsx
+++ week3/src/pages/Market.jsx
@@ -1,83 +1,42 @@
 import CollectionContainer from '../containers/CollectionContainer';
 import ProductContanier from '../containers/ProductContainer';
 import BannerContainer from '../containers/BannerContainer';
-import { useEffect } from 'react';
-// import { useEffect, useState } from 'react';
+// import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Popup from '../components/Popup';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 
 export default function Market() {
-	// const [showMainPop, setShowMainPop] = useState(false);
-	// // 기본 세팅 값은 false(팝업 노출 없음)
-	// // true로 할 경우, '오늘 하루 안 보기'를 클릭해도 리렌더링하면 팝업창이 나타남
-	// const HOME_VISITED = localStorage.getItem('homeVisited');
-	// // localStorage에 homeVisited 조회(가져오기)
+	const [showMainPop, setShowMainPop] = useState(false);
+	// 기본 세팅 값은 false(팝업 노출 없음)
+	// true로 할 경우, '오늘 하루 안 보기'를 클릭해도 리렌더링하면 팝업창이 나타남
+	const HOME_VISITED = localStorage.getItem('homeVisited');
+	// localStorage에 homeVisited 조회(가져오기)
 
-	// useEffect(() => {
-	// 	// 오늘의 시간을 담은 정수
-	// 	const today = new Date();
-	// 	const handleMainPop = () => {
-	// 		if (HOME_VISITED && HOME_VISITED > today) {
-	// 			// localStorage에 저장된 시간이 존재(true)하고 &&
-	// 			// 현재 시간(today)이 localStorage의 시간보다 작으면 return(기본값 false를 return), popup 노출 없음(setShowMainPop(false))
-	// 			// localStorage의 시간: 이전에 팝업창이 떴을 때, '오늘 하루 보지 않기'를 클릭한 시간 + 24시간
-	// 			return;
-	// 		}
-	// 		if (!HOME_VISITED || HOME_VISITED < today) {
-	// 			// localStorage에 저장된 시간이 없거나(false) |
-	// 			// localStorage에 저장된 시간이 현재 시간(today)보다 작다면
-	// 			// popup 노출
-	// 			setShowMainPop(true);
-	// 		}
-	// 	};
-	// 	window.setTimeout(handleMainPop, 100); // 0.1초 후에 실행
-	// }, [HOME_VISITED]);
-
-	const today = new Date();
-
-	// 관리할 상태 정의
-	const initialState = {
-		visited: localStorage.getItem('homeVisited'),
-		boolean: false,
-	};
-
-	// 액션타입 정의
-	const SETSHOWMAINPOP = 'SETSHOWMAINPOP';
-
-	// 액션 생성함수 정의
-	const setShowMainPop = () => ({
-		type: SETSHOWMAINPOP,
-		visited: localStorage.getItem('homeVisited'),
-		boolean: true,
-	});
-
-	// 리듀서 만들기
-	function reducer(state = initialState, action) {
-		if (action.visited && action.visited > today) {
-			return;
-		}
-		if (!action.visited || action.visited < today) {
-			return {
-				...state,
-				boolean: true,
-			};
-		}
-	}
-
-	// 스토어 만들기
-	const store = createStore(reducer);
-
-	// 디스패치
 	useEffect(() => {
-		store.dispatch(window.setTimeout(setShowMainPop, 100));
-	}, [initialState.visited, store]);
-	// store.dispatch(window.setTimeout(setShowMainPop, 100));
+		// 오늘의 시간을 담은 정수
+		const today = new Date();
+		const handleMainPop = () => {
+			if (HOME_VISITED && HOME_VISITED > today) {
+				// localStorage에 저장된 시간이 존재(true)하고 &&
+				// 현재 시간(today)이 localStorage의 시간보다 작으면 return(기본값 false를 return), popup 노출 없음(setShowMainPop(false))
+				// localStorage의 시간: 이전에 팝업창이 떴을 때, '오늘 하루 보지 않기'를 클릭한 시간 + 24시간
+				return;
+			}
+			if (!HOME_VISITED || HOME_VISITED < today) {
+				// localStorage에 저장된 시간이 없거나(false) |
+				// localStorage에 저장된 시간이 현재 시간(today)보다 작다면
+				// popup 노출
+				setShowMainPop(true);
+			}
+		};
+		window.setTimeout(handleMainPop, 100); // 0.1초 후에 실행
+	}, [HOME_VISITED]);
 
 	return (
 		<>
 			{/* Popup에 setShoMainPop전달 */}
-			{/* {showMainPop &&÷<Popup setShowMainPop={setShowMainPop}></Popup>} */}
-			<Popup setShowMainPop={setShowMainPop}></Popup>
+			{showMainPop && <Popup setShowMainPop={setShowMainPop}></Popup>}
 			<div className="shadow"></div>
 			<div className="main">
 				<BannerContainer />
